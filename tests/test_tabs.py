8cf24b512b623f7cd33f08823355041251f917e9
kor0p <viktorua25.03@gmail.com>
2021-02-23 00:29:18 +0200

    upgrade tabs, fix and style example with tabs

--- tests/test_tabs.py
+++ tests/test_tabs.py
@@ -1,4 +1,5 @@
 from pyx import tabs, tab, style
+from flask import request
 
 import tests.test_1 as test_1
 import tests.test_2 as test_2
@@ -6,16 +7,59 @@ import tests.test_3 as test_3
 
 
 def main():
-    test_1_content = test_1.__pyx__()
-    test_2_content = test_2.__pyx__()
-    test_3_content = test_3.__pyx__()
+    tabs_list = [
+        dict(name='page 1', children=test_1.__pyx__(), url='/?page=1'),
+        dict(name='page 2', children=test_2.__pyx__(), url='/?page=2'),
+        dict(name='page 3', children=test_3.__pyx__(), url='/?page=3')
+    ]
     return [
-        tabs(children=[
-            tab(name='page 1', children=test_1_content),
-            tab(name='page 2', children=test_2_content),
-            tab(name='page 3', children=test_3_content),
-        ]),
-        style(scoped=True, children='''// TODO: add styles'''),
+        tabs(
+            _class='content',
+            children=[tab(**kw) for kw in tabs_list]
+        ),
+        style(scoped=True, children='''
+            ul {
+                list-style-type: none;
+                margin: 0;
+                padding: 0;
+                overflow: hidden;
+                background-color: #f1f1f1;
+            }
+            
+            /* Float the list items side by side */
+            tab {
+                float: left;
+            }
+            
+            /* Change background color of links on hover */
+            tabs tab:hover {
+                background-color: #ddd;
+            }
+            
+            /* Create an active/current tablink class */
+            tabs tab:focus, tabs tab[active] li {
+                background-color: #ccc;
+            }
+            
+            /* Style the links inside the list items */
+            li {
+                font-family: "Lato", sans-serif;
+                display: inline-block;
+                color: black;
+                text-align: center;
+                padding: 14px 16px;
+                text-decoration: none;
+                transition: 0.3s;
+                font-size: 17px;
+            }
+            
+            /* Style the tab content */
+            .content {
+                padding: 6px 12px;
+                -webkit-animation: fadeEffect 1s;
+                animation: fadeEffect 1s;
+            }
+        '''),
     ]
 
 
